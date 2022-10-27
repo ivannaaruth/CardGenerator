ed1703fb2d8927d60f2c2cb41de0f56246fe9b47
faidatul <84890931+faidatul@users.noreply.github.com>
2022-10-27 12:37:13 +0700

    make homepage

---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+
+    <h2>Welcome to Food Menu Generator</h2>
+
+
+    <a name="" id="" class="btn btn-primary" role="button">Create Now</a>
+
+</div>
+
+
+@endsection
