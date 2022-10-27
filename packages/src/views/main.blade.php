24b1f407ddd8d8bbbee1b41500a3ff1dfbadff27
faidatul <84890931+faidatul@users.noreply.github.com>
2022-10-27 16:33:19 +0700

    move to package

---
+++ packages/src/views/main.blade.php
@@ -0,0 +1,13 @@
+
+
+<div class="container">
+
+    <h2>Welcome to Food Menu Generator</h2>
+    <h3>How much food that you need to input in one paper?</h3><hr>
+    <form action="" method='POST'>
+        <input type="text" name='foodName' placeholder='number between 3-9'><br></br> -->
+        <a class="btn btn-primary" href="{{ url('input') }}" role="button">Create Now</a>
+    </form>
+    
+
+</div>
