f5ebebc0816f030c34a27607562ffe1907852553
nicolenatsh <nicolenatasha3@gmail.com>
2022-10-27 18:49:14 +0700

    <br>

--- packages/src/views/main.blade.php
+++ packages/src/views/main.blade.php
@@ -7,9 +7,9 @@
 
 
    <form action="" method='POST'>
-   <input type="text" name='foodName' placeholder='food name'>
-   <input type="text" name='foodDesc' placeholder='food description'>
-   <input type="text" name='price' placeholder='price'>
+   <input type="text" name='foodName' placeholder='food name'><br>
+   <input type="text" name='foodDesc' placeholder='food description'><br>
+   <input type="text" name='price' placeholder='price'><br>
    <input type="text" name='compo' placeholder='main composition'><br>
    <input type="file" name='pict'><hr>
    <a class="btn btn-primary" href="{{ url('/result') }}"  role="button">Create Now</a>
