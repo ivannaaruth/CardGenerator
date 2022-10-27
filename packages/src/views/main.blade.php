6fe76c629289b3e07133b4d51b4a94c962e06333
nicolenatsh <nicolenatasha3@gmail.com>
2022-10-27 19:10:29 +0700

    Realtime Preview Text only

--- packages/src/views/main.blade.php
+++ packages/src/views/main.blade.php
@@ -6,14 +6,46 @@
     <h4>You Can Fill the Data of Your Food</h4><hr>
 
 
-   <form action="" method='POST'>
-   <input type="text" name='foodName' placeholder='food name'><br>
-   <input type="text" name='foodDesc' placeholder='food description'><br>
-   <input type="text" name='price' placeholder='price'><br>
-   <input type="text" name='compo' placeholder='main composition'><br>
-   <input type="file" name='pict'><hr>
-   <a class="btn btn-primary" href="{{ url('/result') }}"  role="button">Create Now</a>
+    <form action="" method='POST'>
 
+    <input type="text" id="name" oninput="funcName()" name='foodName' placeholder='food name'><br>
+    <p id="pName"></p>
+    <script>
+        function funcName() {
+            var name = document.getElementById("name").value;
+            document.getElementById("pName").innerHTML = "Preview Food Name: " + name;
+        }
+    </script>
+    
+    <input type="text" id="desc" oninput="funcDesc()" name='foodDesc' placeholder='food description'><br>
+    <p id="pDesc"></p>
+    <script>
+        function funcDesc() {
+            var desc = document.getElementById("desc").value;
+            document.getElementById("pDesc").innerHTML = "Preview Food Desc: " + desc;
+        }
+    </script>
+
+    <input type="text" id="price" oninput="funcPrice()" name='price' placeholder='price'><br>
+    <p id="pPrice"></p>
+    <script>
+        function funcPrice() {
+            var price = document.getElementById("price").value;
+            document.getElementById("pPrice").innerHTML = "Preview Food Price: " + price;
+        }
+    </script>
+
+    <input type="text" id="comp" oninput="funcComp()" name='compo' placeholder='main composition'><br>
+    <p id="pComp"></p>
+    <script>
+        function funcComp() {
+            var comp = document.getElementById("comp").value;
+            document.getElementById("pComp").innerHTML = "Preview Compotition: " + comp;
+        }
+    </script>
+
+    <input type="file" name='pict'><hr>
+    <a class="btn btn-primary" href="{{ url('/result') }}"  role="button">Create Now</a>
 
     </form>
   
