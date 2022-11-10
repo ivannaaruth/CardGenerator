2819a3814e91a9a60c495602ab5b78ab69acd6af
faidatul <84890931+faidatul@users.noreply.github.com>
2022-11-10 13:46:18 +0700

    merapikan views

--- packages/src/views/main.blade.php
+++ packages/src/views/main.blade.php
@@ -1,84 +1,126 @@
 <html lang="en">
 <head>
     <title>Card Item Maker</title>
-    <link rel="stylesheet" href="preview.css">
+    <link rel="stylesheet" href="display.css">
+    <link rel="stylesheet" href="card.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>
+    <script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
+
 </head>
 <body>
-
-<div class="container">
-
-    <h2>Welcome to Card Item Generator</h2>
-    <h4>You Can Fill the Data of Your Item</h4><hr>
-
-    <form action="" method='POST'>
-
-    <label>Item Name</label><br>
-    <input type="text" id="name" oninput="funcName()" name='foodName' placeholder='item name'><br>
-    
-    <label>Item Description</label><br>
-    <!-- <input type="text" id="desc" oninput="funcDesc()" name='foodDesc' placeholder='food description'><br> -->
-    <textarea id="desc" oninput="funcDesc()" name='foodDesc' placeholder='item description' wrap="soft"></textarea><br>
-    
-    <label>Item Price</label><br>
-    <input type="text" id="price" oninput="funcPrice()" name='price' placeholder='price'><br>
-
-    <label>Compotition</label><br>
-    <input type="text" id="comp" oninput="funcComp()" name='compo' placeholder='composition'><br>
-
-    <label>Item Image</label><br>
-    <input type="file" id="img" accept="image/*" onchange="loadFile(event)" name='pict'><br>
-    
-    <a class="btn btn-primary" href="{{ url('/result') }}"  role="button">Create Now</a>
-
-    </form>
-</div>
-
-<div class="overlap-group">
-
-    <img id="pImg"/>
-    <script>
-        var loadFile = function(event){
-            var img = document.getElementById('pImg');
-            img.src = URL.createObjectURL(event.target.files[0]);
-            img.onload = function() {
-            URL.revokeObjectURL(img.src) // free memory
-            }
-        }
-    </script>
-
-    <p id="pName"></p>
-    <script>
-        function funcName() {
-            var name = document.getElementById("name").value;
-            document.getElementById("pName").innerHTML = "Preview Item Name: " + name;
-        }
-    </script>
-
-    <p id="pDesc"></p>
-    <script>
-        function funcDesc() {
-            var desc = document.getElementById("desc").value;
-            document.getElementById("pDesc").innerHTML = "Preview Item Desc: " + desc;
-        }
-    </script>
-
-    <p id="pPrice"></p>
-    <script>
-        function funcPrice() {
-            var price = document.getElementById("price").value;
-            document.getElementById("pPrice").innerHTML = "Preview Item Price: " + price;
-        }
-    </script>
-
-    <p id="pComp"></p>
-        <script>
-        function funcComp() {
-            var comp = document.getElementById("comp").value;
-            document.getElementById("pComp").innerHTML = "Preview Compotition: " + comp;
-        }
-        </script>
-
+<div class="form-body">
+        <div class="row">
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <h3>Welcome to Card Maker</h3>
+                        <p>Fill in the data below.</p>
+                            <div class="col-md-12">
+                               <input class="form-control" type="text" id="Name" oninput="funcName()" placeholder='item name'>
+                            </div>
+                            <div class="col-md-12">
+                               <input class="form-control" type="text"  id="price" oninput="funcPrice()" placeholder='price'>
+                            </div>
+                            <div class="col-md-12">
+                               <input class="form-control" type="text" id="desc" oninput="funcDesc()" placeholder='item description' wrap="soft">
+                            </div>
+                            <div class="col-md-8">
+                            <br><input class="form-control" type="file" id="img" accept="image/*" onchange="loadFile(event)">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="form-items">
+                    <h3>Here the Results</h3>
+                        <div class="container mx-auto mt-4">
+                            <div class="row">
+                                <div class="col-md-4" >
+                                    <div class="card" style="width: 12rem;" id="content">
+                                        <img src="https://t3.ftcdn.net/jpg/02/18/21/86/360_F_218218632_jF6XAkcrlBjv1mAg9Ow0UBMLBaJrhygH.jpg" class="card-img-top" id = "pImg">
+                                        <div class="card-body">
+                                            <h5 class="card-title" id = "pName">Item Name</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted" id = "pPrice">Item Price</h6>
+                                            <h6 id = "pDesc">Item Description</h6>
+                                        </div>
+                                    </div> 
+                                <div id="previewImg" style="display:none"></div>
+                                <br><button id="download" class="btn-primary">Download</button> 
+                                </div> 
+                                <div class="col-md-4 mx-auto" >
+                                    <div class="card" style="width: 12rem;" id="content2">
+                                        <img src="https://t3.ftcdn.net/jpg/02/18/21/86/360_F_218218632_jF6XAkcrlBjv1mAg9Ow0UBMLBaJrhygH.jpg" class="card-img-top" id = "pImg">
+                                        <div class="card-body">
+                                            <h5 class="card-title" id = "pName">Item Name</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted" id = "pPrice">Item Price</h6>
+                                            <h6 id = "pDesc">Item Description</h6>
+                                        </div>
+                                    </div> 
+                                <div id="previewImg2" style="display:none"></div>
+                                <br><button id="download2" class="btn-primary">Download</button> 
+                                </div> 
+                            </div>
+                        </div>
+                    </div>   
+                </div>
+            </div>
+        </div>
     </div>
+
+<script>
+     document.getElementById("download").addEventListener("click", function() {
+	html2canvas(document.getElementById("content")).then(function (canvas) {			
+        var anchorTag = document.createElement("a");
+			document.body.appendChild(anchorTag);
+			document.getElementById("previewImg").appendChild(canvas);
+			anchorTag.download = "filename.jpg";
+			anchorTag.href = canvas.toDataURL();
+			anchorTag.target = '_blank';
+			anchorTag.click();
+		});
+ });
+</script>
+
+<script>
+     document.getElementById("download2").addEventListener("click", function() {
+	html2canvas(document.getElementById("content2")).then(function (canvas) {			
+        var anchorTag = document.createElement("a");
+			document.body.appendChild(anchorTag);
+			document.getElementById("previewImg2").appendChild(canvas);
+			anchorTag.download = "filename.jpg";
+			anchorTag.href = canvas.toDataURL();
+			anchorTag.target = '_blank';
+			anchorTag.click();
+		});
+ });
+</script>
+
+<script>
+    var loadFile = function(event){
+    var img = document.getElementById('pImg');
+    img.src = URL.createObjectURL(event.target.files[0]);
+    img.onload = function() {
+    URL.revokeObjectURL(img.src)}
+    }
+</script>
+<script>
+    function funcName() {
+    var name = document.getElementById("Name").value;
+    document.getElementById("pName").innerHTML = name;
+    }
+</script>
+<script>
+    function funcDesc() {
+    var desc = document.getElementById("desc").value;
+    document.getElementById("pDesc").innerHTML = desc;
+    }
+</script>
+<script>
+    function funcPrice() {
+    var price = document.getElementById("price").value;
+    document.getElementById("pPrice").innerHTML = "Rp. "+  price;
+    }
+</script>
 
 </body>
 </html>
