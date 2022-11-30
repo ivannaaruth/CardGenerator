89ae0ee391fd702a5b07d1e9730447c041a664cb
faidatul <84890931+faidatul@users.noreply.github.com>
2022-11-30 22:54:31 +0700

    Update CardServiceProvider.php

--- CardItemMaker/packages/src/CardServiceProvider.php
+++ CardItemMaker/packages/src/CardServiceProvider.php
@@ -25,6 +25,7 @@ class CardServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom( path:  __DIR__ . '/route/web.php');
         $this->loadViewsFrom( path:  __DIR__ . '/views', namespace: 'CardMaker');
+        //publishes
         $this->publishes([
             __DIR__.'/views' => resource_path('views/vendor/cardmaker'),
             __DIR__.'/Http/Controllers/CardController.php' => app_path('/Http/Controllers/CardController.php')
